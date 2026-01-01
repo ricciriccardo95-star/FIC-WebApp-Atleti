9f18cd0294d37a689f1337c2eabb4c7b7d455355
ricciriccardo95-star <ricci.riccardo95@gmail.com>
2026-01-01 12:59:12 +0100

    Update cache name to version 1.59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fic-pwa-cache-v1.58';
+const CACHE_NAME = 'fic-pwa-cache-v1.59';
 
 // Lista dei file fondamentali dell'applicazione da mettere in cache.
 // Aggiungi qui tutte le pagine e le risorse principali della tua PWA.
